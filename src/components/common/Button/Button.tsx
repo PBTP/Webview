60c9fad395e399db42580390a5042f0f4bcecb1b
winju <winju@wemade.com>
2024-03-27 00:48:56 +0900

    [feat] added disabled logic to Button

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,14 +2,19 @@ import React, { ReactNode } from 'react';
 import styles from './Button.module.scss';
 
 type ButtonProps = {
-  buttonType: 'Gray' | 'White' | 'Primary' | 'disabled';
+  buttonType: 'Gray' | 'White' | 'Primary' | 'Disabled' | 'Reserved';
   children?: ReactNode;
   className?: string;
 };
 
 const Button = ({ children, buttonType, className }: ButtonProps) => {
   return (
-    <button className={`${className} ${styles[buttonType]}`}>{children}</button>
+    <button
+      disabled={['Disabled', 'Reserved'].includes(buttonType)}
+      className={`${className} ${styles[buttonType]}`}
+    >
+      {children}
+    </button>
   );
 };
 
