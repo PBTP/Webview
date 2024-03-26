c7a774616f81e0ead28008253001c246a898ea5c
winju <winju@wemade.com>
2024-03-27 00:40:29 +0900

    [styles] styled the DetailReservation

--- src/pages/Rservation/DetatilReservation/DetailReservation.tsx
+++ src/pages/Rservation/DetatilReservation/DetailReservation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import styles from './DetailRservation.module.scss';
 import ContentField from '@/components/common/ContentField/ContentField';
+import Button from '@/components/common/Button/Button';
 
 const DetailReservation = () => {
   // const location = useLocation();
@@ -38,62 +39,84 @@ const DetailReservation = () => {
           <input placeholder="상세주소 입력" />
         </ContentField>
       </div>
-      <div>
-        <div>
+      <div className={styles.ParkingCheckBoxWrapper}>
+        <div className={styles.ParkingCheckBoxContainer}>
           <div>체크박스</div>
-          <div>주차 공간이 마련되어 있습니다.</div>
+          <div className={styles.CheckBoxText}>
+            주차 공간이 마련되어 있습니다.
+          </div>
         </div>
-        <div>
+        <div className={styles.ParkingDescription}>
           위 내용을 사실과 다르게 기재한 경우, 서비스 이용에 제한이 있을 수
           있습니다.
         </div>
       </div>
-      <div>
-        <div>
-          <div>반려 동물 정보</div>
-          <button>추가하기</button>
+      <div className={styles.ParentInfoWrapper}>
+        <div className={styles.Title}>보호자 연락처</div>
+        <ContentField className={styles.Telecom} backgroundColor="Gray">
+          <input placeholder="통신사 선택" />
+        </ContentField>
+        <ContentField className={styles.Phone} backgroundColor="Gray">
+          <input placeholder="010-0000-0000" />
+        </ContentField>
+      </div>
+      <div className={styles.PetInfoWrapper}>
+        <div className={styles.Header}>
+          <div className={styles.Title}>반려 동물 정보</div>
+          <Button className={styles.AddButton} buttonType="Gray">
+            추가하기
+          </Button>
         </div>
         {/* 있을떄 */}
-        <div>반려 동물을 등록해보세요.</div>
-        <button>등록하기</button>
+        <div className={styles.PetAddWrapper}>
+          <div className={styles.PetAddDescription}>
+            반려 동물을 등록해보세요.
+          </div>
+          <Button className={styles.RegistButton} buttonType="Gray">
+            등록하기
+          </Button>
+        </div>
         {/* 없을때 */}
-        <div>
-          <div>강아지사진</div>
-          <div>
-            <div>
+        <ContentField
+          backgroundColor="Gray"
+          className={styles.PetInfoContainer}
+        >
+          <div className={styles.PetPhoto}>강아지사진</div>
+          <div className={styles.PetInfoBox}>
+            <div className={styles.PetNameLine}>
               <div>
-                <span>김뽀삐</span>
+                <span className={styles.Name}>김뽀삐</span>
                 <span>성별 아이콘</span>
               </div>
               <div>수정(연필) 아이콘</div>
             </div>
-            <div>
+            <div className={styles.DetailInfoLine}>
               <div>시고르자브종</div>
-              <div>|</div>
+              <div className={styles.VerticalDivier} />
               <div>3년 4개월</div>
-              <div>|</div>
+              <div className={styles.VerticalDivier} />
               <div>3kg</div>
             </div>
           </div>
-        </div>
-        <div>
-          <div>
-            <div>성향 체크</div>
-            <div>완료</div>
-          </div>
-          <div>
-            <div>접종 및 건강</div>
-            <div>완료</div>
-          </div>
+        </ContentField>
+        <div className={styles.HealthWrapper}>
+          <ContentField backgroundColor="Gray" className={styles.HealthInfo}>
+            <div className={styles.Type}>성향 체크</div>
+            <div className={styles.Status}>완료</div>
+          </ContentField>
+          <ContentField backgroundColor="Gray" className={styles.HealthInfo}>
+            <div className={styles.Type}>접종 및 건강</div>
+            <div className={styles.Status}>완료</div>
+          </ContentField>
         </div>
       </div>
-      <div>
-        <div>업체 규정 확인</div>
-        <div>
+      <div className={styles.BusinessRulesWrapper}>
+        <div className={styles.Title}>업체 규정 확인</div>
+        <div className={styles.RulesCheckBoxWrapper}>
           <div>체크박스</div>
           <div>업체 규정 및 약관을 확인했습니다.</div>
         </div>
-        <div>
+        <div className={styles.RulesDescription}>
           위 내용을 사실과 다르게 기재한 경우, 서비스 이용에 제한이 있을 수
           있습니다.
         </div>
