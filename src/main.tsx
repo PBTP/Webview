4e9cc46dd774e26941b2784cb0bcd9eb4ef7b638
gbwlxhd97 <gbwlxhd97@naver.com>
2024-03-30 23:49:10 +0900

    feat : msw setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/styles/global.scss';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RootRouter } from './router/RootRouter';
+import initMocks from '@/mocks/index.js';
+
+if (import.meta.env.DEV) {
+  await initMocks();
+}
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
