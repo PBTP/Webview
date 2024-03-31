550f2f26335b2a93293d8f7b5b090c9a7d543924
winju <winju@wemade.com>
2024-03-31 22:52:11 +0900

    [styles] add chatItem styles

--- src/router/RootRouter.tsx
+++ src/router/RootRouter.tsx
@@ -1,5 +1,6 @@
 import App from '@/App';
 import RootLayout from '@/components/Layout/RootLayout/RootLayout';
+import ChatRoomList from '@/pages/Chat/ChatRoomList/ChatRoomList';
 import DetailReservation from '@/pages/Rservation/DetatilReservation/DetailReservation';
 import {
   createBrowserRouter,
@@ -12,6 +13,7 @@ export const RootRouter = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
+        <Route path="chat-list" element={<ChatRoomList />} />
         <Route path="reservation" element={<App />} />
         <Route path="reservation/*" element={<DetailReservation />} />
       </Route>
