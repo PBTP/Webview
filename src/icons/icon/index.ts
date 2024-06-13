20b67f748a6980702b4f470770af7fbfb41c2ad6
VictoryJu <kob0805@naver.com>
2024-06-14 00:27:31 +0900

    [styles] add icons

--- src/icons/icon/index.ts
+++ src/icons/icon/index.ts
@@ -2,3 +2,5 @@ export { default as ArrowRightIcon } from './ArrowRightIcon';
 export { default as CalendarIcon } from './CalendarIcon';
 export { default as MarkerIcon } from './MarkerIcon';
 export { default as CheckboxIcon } from './CheckboxIcon';
+export { default as SearchIcon } from './SearchIcon';
+export { default as TargetIcon } from './TargetIcon';
