519cd63e83387b0f1ad5f608c3606d718099e633
VictoryJu <kob0805@naver.com>
2024-07-13 14:14:08 +0900

    [styels]: ContentField border, focust style

--- src/components/common/ContentField/ContentField.tsx
+++ src/components/common/ContentField/ContentField.tsx
@@ -5,19 +5,20 @@ import styles from './ContentField.module.scss';
 type ContentFieldProps = {
   children: ReactNode;
   backgroundColor: 'Gray' | 'White';
+  border?: 'Border' | 'None';
   className?: string;
   onClick?: () => void;
 };
 
 const ContentField = forwardRef<HTMLDivElement, ContentFieldProps>(
   (
-    { children, backgroundColor, className, onClick },
+    { children, backgroundColor, border = 'None', className, onClick },
     ref: ForwardedRef<HTMLDivElement>
   ) => {
     return (
       <div
         ref={ref}
-        className={`${styles.Wrapper} ${styles[backgroundColor]} ${className}`}
+        className={`${styles.ContentFieldWrapper} ${styles[backgroundColor]} ${styles[border]} ${className}`}
         onClick={onClick}
       >
         {children}
