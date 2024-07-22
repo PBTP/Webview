5b13f4e47962e990a966ed6680074aff48667a34
winju <winju@wemade.com>
2024-07-22 16:15:22 +0900

    [fix]: ProtectedRoute delete

--- src/router/RootRouter.tsx
+++ src/router/RootRouter.tsx
@@ -11,10 +11,8 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom';
-import ProtectedRoute from './ProtectedRoute';
-import { setAccessToken, useTokenStore } from '@/stores/useTokenStore';
+import { setAccessToken } from '@/stores/useTokenStore';
 import Location from '@/pages/Location/Location/Location';
-import { requestAPI } from '@/utils/fetch';
 import ReviewPage from '@/pages/Review/Review';
 
 export const RootRouter = () => {
@@ -27,18 +25,16 @@ export const RootRouter = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
-        <Route element={<ProtectedRoute />}>
-          <Route path="chat-list" element={<CharRoomsPage />} />
-          <Route path="reservation" element={<ReservationIndexPage />} />
-          <Route path="chat-list/:chatId" element={<ChatRoomPage />} />
-          <Route
-            path="reservation/:reservationId"
-            element={<DetailReservation />}
-          />
-          <Route path="payment" element={<PaymentPage />} />
-          <Route path="review" element={<ReviewPage />} />
-          <Route path="location" element={<Location />} />
-        </Route>
+        <Route path="chat-list" element={<CharRoomsPage />} />
+        <Route path="reservation" element={<ReservationIndexPage />} />
+        <Route path="chat-list/:chatId" element={<ChatRoomPage />} />
+        <Route
+          path="reservation/:reservationId"
+          element={<DetailReservation />}
+        />
+        <Route path="payment" element={<PaymentPage />} />
+        <Route path="review" element={<ReviewPage />} />
+        <Route path="location" element={<Location />} />
       </Route>
     )
   );
